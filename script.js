6da2642c527f21e6604accce3bbbda210960285f
Edia Zaki <13521141@mahasiswa.itb.ac.id>
2023-04-12 01:30:58 +0700

    fix: ucs

--- script.js
+++ script.js
@@ -1,144 +1,3 @@
-function Path(parentVal, currentVal, valueVal){
-    return {parent: parentVal, current: currentVal, value:valueVal};
-    // constructor(parent, current, value){
-    //     // Type Path
-    //     this.parent = parent;
-        
-    //     this.current = current;
-    //     this.value = value;
-    // }
-}
-
-class PrioQueue{
-    constructor(){
-        //Type Path
-        this.elements = [];
-        
-        this.size = 0;
-    }
-
-    getHead(){
-        return this.elements[0];
-    }
-
-    enqueue(element){
-        if(this.size == 0){
-            this.elements.push(element);
-        }else{
-            var pos = -1;
-            for(let i = 0; i < this.size; i++){
-                if(this.elements[i].value > element.value){
-                    pos = i;
-                    break;
-                }
-            }
-
-            // If pos is at the end or not
-            if(pos == -1){
-                this.elements.push(element);
-            }else{
-                for(let i = this.size; i > pos; i--){
-                    if(i == this.size){
-                        this.elements.push(this.elements[this.size - 1]);
-                    }else{
-                        this.elements[i] = this.elements[i - 1];
-                    }
-                }
-            }
-            
-            // Insert element at pos
-            this.elements[pos] = element;
-        }
-        this.size++;
-    }
-
-    dequeue(){
-        console.log("DEQUEUE DEBUG#1: " + this.elements[0]);
-        var element = this.elements[0];
-        
-        for(let i = 0; i < this.size - 1; i++){
-            this.elements[i] = this.elements[i + 1];
-        }
-
-        this.elements.pop();
-
-        //console.log("DEQUEUE DEBUG: " + element.current);
-
-        return element;
-    }
-}
-
-function createGraphMap(graph){
-    var graphMap = {};
-    for(let i = 0; i < graph.length; i++){
-        var current = graph[i];
-        // First
-        if(graphMap[current[0]] == null){
-            graphMap[current[0]] = {};
-        }
-        if(graphMap[current[1]] == null){
-            graphMap[current[1]] = {};
-        }
-        graphMap[current[0]][current[1]] = current[2];
-        graphMap[current[1]][current[0]] = current[2];
-    }
-    return graphMap;
-}
-
-
-
-function search(graphMap, start, end, method){
-    var queue = new PrioQueue();
-    var temp = new Path(null, start, 0);
-    console.log("Value: " + temp.value);
-    queue.enqueue(temp);
-
-    var flag = false;
-    var history = [];
-
-    while(queue.size > 0){
-        var active = queue.dequeue();
-        console.log(active);
-        history.push(active.current);
-
-        if(active.current == end){
-            return active;
-        }else{
-            var nextNodes = possibleNodes(graphMap, active);
-            for(let i = 0; i < nextNodes.length; i++){
-                if(!isExist(history, active)){
-                    queue.enqueue(new Path(active, nextNodes[i].current, nextNodes[i].value + active.value));
-                }
-            }
-        }
-    }
-}
-
-function possibleNodes(graphMap, currentNode){
-    var possibilities = [];
-    var node = graphMap[currentNode];
-    for(let i in node){
-        possibilities.push({currentNode: i, value: node[i]})
-    }
-    return possibilities;
-}
-
-function writeResult(resultPath){
-    if(resultPath.parent != null){
-        writeResult(resultPath.parent);
-        console.log(" => " + resultPath.current + "(" + resultPath.value + ")");
-    }else{
-        console.log(resultPath.current + "(" + resultPath.value + ")");
-    }
-}
-
-function isExist(history, node){
-    for(let i = 0; i < history.length; i++){
-        if(history[i] == node) return true;
-    }
-    return false;
-}
-
 var testGraph = [
     ["oradea"         , "zerind"         , 71],
     ["oradea"         , "sibiu"          , 151],
@@ -165,4 +24,163 @@ var testGraph = [
     ["iasi"           , "neamt"          , 87]
 ];
 
-writeResult(search(createGraphMap(testGraph), "arad", "bucharest", "UCS"));
+result("arad", "bucharest", testGraph, createGraphMap, "UCS");
+
+function createPath(parent, current, value) {
+    return {
+        parent : parent,
+        current: current,
+        value: value
+    };
+};
+
+class Path{
+    //return {parent: parentVal, current: currentVal, value:valueVal};
+    constructor(parent, current, value){
+        // Type Path
+        this.parent = parent;
+        
+        this.current = current;
+        this.value = value;
+    }
+}
+
+function isExist(history, node){
+    for(let i = 0; i < history.length; i++){
+        if(history[i] == node) return true;
+    }
+    return false;
+}
+
+function createGraphMap(graph){
+    var graphMap = {};
+    for(let i = 0; i < graph.length; i++){
+        var current = graph[i];
+        // First
+        if(graphMap[current[0]] == null){
+            graphMap[current[0]] = {};
+        }
+        if(graphMap[current[1]] == null){
+            graphMap[current[1]] = {};
+        }
+        graphMap[current[0]][current[1]] = current[2];
+        graphMap[current[1]][current[0]] = current[2];
+    }
+    return graphMap;
+}
+
+function compute(start, end, graphMap, func, method){
+    function writeResult(resultPath){
+        if(resultPath.parent != null){
+            writeResult(resultPath.parent);
+            console.log(" => " + resultPath.current + "(" + resultPath.value + ")");
+        }else{
+            console.log(resultPath.current + "(" + resultPath.value + ")");
+        }
+    }
+
+    writeResult(func(graphMap, start, end, method));
+}
+
+function result(start, end, graph, func, method){
+
+    function search(graphMap, start, end, method){
+        function dequeue(array){
+            var element = array[0];
+                
+            for(let i = 0; i < array.length - 1; i++){
+                array[i] = array[i + 1];
+            }
+            array.pop();
+        
+            return element;
+        }
+        
+        function enqueue(array, element){
+            if(array.length == 0){
+                array.push(element);
+            }else{
+                var pos = -1;
+                for(let i = 0; i < array.length; i++){
+                    if(method == "UCS" && array[i].value > element.value){
+                        pos = i;
+                        break;
+                    }
+                    // Diganti euclidean
+                    else if(method == "A" && array[i].value > element.value){
+                        pos = i;
+                        break;
+                    }
+                }
+        
+                // If pos is at the end or not
+                if(pos == -1){
+                    array.push(element);
+                }else{
+                    for(let i = array.length; i > pos; i--){
+                        if(i == array.length){
+                            array.push(array[array.length - 1]);
+                        }else{
+                            array[i] = array[i - 1];
+                        }
+                    }
+                }
+                
+                // Insert element at pos
+                array[pos] = element;
+            }
+        }
+
+        function possibleNodes(graphMap, currentNode){
+            var possibilities = [];
+            var node = graphMap[currentNode];
+            for(var i in node){
+                possibilities.push({current: i, value: node[i]});
+            }
+            return possibilities;
+        }
+
+
+        var queue = [];
+        //enqueue(queue, createPath(null, start, 0))
+        queue.push(createPath(null, start, 0));
+        var head = 0;
+        //enqueue(queue, createPath(null, start, 0));
+    
+        var history = [];
+    
+        while(queue.length - head != 0){
+            var active = queue[head];
+            head++;
+            // console.log("Current active: ");
+            // console.log(active);
+            history.push(active.current);
+    
+            if(active.current == end){
+                //console.log("FINISH");
+                return active;
+            }else{
+                // console.log("Queue: " + queue);
+                // console.log("History: " + history);
+                // console.log("not finished");
+                var nextNodes = possibleNodes(graphMap, active.current);
+                // console.log("Possibilities calculated:");
+                // console.log(nextNodes);
+                for(let i = 0; i < nextNodes.length; i++){
+                    if(!isExist(history, active)){
+                        //console.log("New node acquired");
+                        //queue.push(createPath(active, nextNodes[i].current, nextNodes[i].value + active.value));
+                        enqueue(queue, createPath(active, nextNodes[i].current, nextNodes[i].value + active.value));
+                        //console.log(queue);
+                        //queue.enqueue(new Path(active, nextNodes[i].current, nextNodes[i].value + active.value));
+                        //console.log("MASUK");
+                    }
+    
+                }
+            }
+        }
+    }
+
+    compute(start, end, func(graph), search, method);
+
+}
