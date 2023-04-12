23273da1a57036a626a674e5971f134f8257c311
Edia Zaki <13521141@mahasiswa.itb.ac.id>
2023-04-12 13:09:40 +0700

    feat: a*

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ var testGraph = [
     ["mehadia"        , "drobeta"        , 75],
     ["drobeta"        , "craiova"        , 120],
     ["craiova"        , "pitesti"        , 138],
-    ["craiova"        , "rimnicu vilcea" , 146],
-    ["sibiu"          , "rimnicu vilcea" , 80],
-    ["rimnicu vilcea" , "pitesti"        , 97],
+    ["craiova"        , "rimnicu_vilcea" , 146],
+    ["sibiu"          , "rimnicu_vilcea" , 80],
+    ["rimnicu_vilcea" , "pitesti"        , 97],
     ["sibiu"          , "fagaras"        , 99],
     ["fagaras"        , "bucharest"      , 211],
     ["pitesti"        , "bucharest"      , 101],
@@ -24,10 +24,53 @@ var testGraph = [
     ["iasi"           , "neamt"          , 87]
 ];
 
-var matrix = readFile('test.txt');
-result("arad", "bucharest", testGraph, createGraphMap, "UCS");
+var nameTest = ["oradea", "zerind", "sibiu", "arad", "timisoara", "lugoj", "mehadia", "drobeta", 
+                "craiova", "pitesti", "rimnicu_vilcea", "fagaras", "bucharest", "giurgiu", "urziceni",
+                "hirsova", "eforie", "vaslui", "iasi", "neamt"];
 
-function readFile(filepath){
+var eucToGoalTest = {"arad": 366, "bucharest": 0, "craiova": 160, "dobreta": 242, "eforie": 161, 
+                    "fagaras": 176, "giurgiu": 77, "hirsova": 151, "iasi": 226, "lugoj": 244, "mehadia": 241,
+                    "neamt": 234, "oradea": 380, "pitesti": 10, "rimnicu_vilcea": 193, "sibiu": 253, "timisoara": 329,
+                    "urziceni": 80, "vaslui": 199, "zerind": 374};
+
+const prompt = require('prompt-sync')();
+
+
+
+// Initialize information matrices
+//var matrix = getMatrix('test.txt');
+var nodeNames = nameTest;//getNames('test.txt');
+var nodeCoordinates = [];
+var eucToGoal = eucToGoalTest;//{};
+
+// Insert coordinates of all nodes
+// for(let i = 0; i < nodeNames.length; i++){
+//     var coorX = prompt(`Coordinate X of ${nodeNames[i]}: `);
+//     var coorY = prompt(`Coordinate Y of ${nodeNames[i]}: `);
+//     nodeCoordinates.push({name: nodeNames[i], X: Number(coorX), Y: Number(coorY)});
+// }
+
+// Get begin and goal nodes
+var begin = prompt("Start node: ");
+var goal = prompt('Goal node: ');
+
+// Get array of euclidean distances to goal
+// for(let i = 0; i < nodeNames.length; i++){
+//     eucToGoal[nodeNames[i]] = euclidean(nodeCoordinates[nodeNames.indexOf(nodeNames[i])], nodeCoordinates[nodeNames.indexOf(goal)]);
+//     //eucToGoal.push({name: nodeNames[i], euclidean: euclidean(nodeCoordinates[nodeNames.indexOf(nodeNames[i])], nodeCoordinates[nodeNames.indexOf(goal)])});
+// }
+//var goalCoor = nodeCoordinates[nodeNames.indexOf(goal)];
+
+var chosenMethod = prompt("What method do you want to use?");
+result(begin, goal, testGraph, createGraphMap, chosenMethod);
+
+function euclidean(start, end){
+    let Xelement = Math.pow(Math.abs(start.X - end.X), 2);
+    let Yelement = Math.pow(Math.abs(start.Y - end.Y), 2);
+    return (Math.sqrt(Xelement + Yelement));
+}
+
+function getMatrix(filepath){
     const fs = require('fs');
     var matrix = [];
     console.log(matrix[0]);
@@ -59,6 +102,31 @@ function readFile(filepath){
     return matrix;
 }
 
+function getNames(filepath){
+    const fs = require('fs');
+    var matrix = [];
+    console.log(matrix[0]);
+    const allFileContents = fs.readFileSync(filepath, 'utf-8');
+    var i = 0;
+    var nameArray = [];
+    var item = 0;
+    console.log(allFileContents);
+    allFileContents.split(/\r?\n/).forEach(line =>  {
+        var textLine = line.split(' ');
+        console.log(`Line from file: ${textLine}`);
+        for(let j = 0; j < textLine.length; j++){
+            var Insert = [];
+            if(i == 0){
+                nameArray[j] = textLine[j];
+                console.log(nameArray);
+            }
+        }
+        i++;
+    });
+
+    return nameArray;
+}
+
 function createPath(parent, current, value) {
     return {
         parent : parent,
@@ -67,20 +135,9 @@ function createPath(parent, current, value) {
     };
 };
 
-class Path{
-    //return {parent: parentVal, current: currentVal, value:valueVal};
-    constructor(parent, current, value){
-        // Type Path
-        this.parent = parent;
-        
-        this.current = current;
-        this.value = value;
-    }
-}
-
 function isExist(history, node){
     for(let i = 0; i < history.length; i++){
-        if(history[i] == node) return true;
+        if(history[i] == node.current) return true;
     }
     return false;
 }
@@ -116,7 +173,6 @@ function compute(start, end, graphMap, func, method){
 }
 
 function result(start, end, graph, func, method){
-
     function search(graphMap, start, end, method){
         function dequeue(array){
             var element = array[0];
@@ -135,12 +191,16 @@ function result(start, end, graph, func, method){
             }else{
                 var pos = -1;
                 for(let i = 0; i < array.length; i++){
-                    if(method == "UCS" && array[i].value > element.value){
+                    if(chosenMethod == "UCS" && array[i].value > element.value){
                         pos = i;
                         break;
                     }
+                    
                     // Diganti euclidean
-                    else if(method == "A" && array[i].value > element.value){
+                    else if(chosenMethod == "A*" && array[i].value + eucToGoal[array[i].current] > element.value + eucToGoal[element.current]){
+                        
+                        //console.log(`${element.current} is more little than ${array[i].current} because`);
+                        //console.log(`${element.value + eucToGoal[element.current]} < ${array[i].value + eucToGoal[array[i].current]}`);
                         pos = i;
                         break;
                     }
@@ -175,42 +235,48 @@ function result(start, end, graph, func, method){
 
 
         var queue = [];
-        //enqueue(queue, createPath(null, start, 0))
         queue.push(createPath(null, start, 0));
         var head = 0;
-        //enqueue(queue, createPath(null, start, 0));
-    
-        var history = [];
-    
-        while(queue.length - head != 0){
-            var active = queue[head];
-            head++;
-            // console.log("Current active: ");
-            // console.log(active);
+        console.log("START QUEUE");
+        console.log(queue);
+        var history = []
+        //console.log(queue);
+        while(queue.length > 0){
+            var active = queue[0];
+            console.log("CURRENTLY ACTIVE");
+            console.log(active.current);
+            queue.splice(0, 1);
+            //head++;
+
+            //head++;
+            // console.log(active.current);
+            // console.log(head);
             history.push(active.current);
+
+            // if(head == 5){
+            //     console.log(queue);
+            // }
     
             if(active.current == end){
-                //console.log("FINISH");
                 return active;
             }else{
-                // console.log("Queue: " + queue);
-                // console.log("History: " + history);
-                // console.log("not finished");
                 var nextNodes = possibleNodes(graphMap, active.current);
-                // console.log("Possibilities calculated:");
-                // console.log(nextNodes);
+                console.log("POSSIBLE NEXT");
+                console.log(nextNodes);
+                //console.log(history);
                 for(let i = 0; i < nextNodes.length; i++){
-                    if(!isExist(history, active)){
-                        //console.log("New node acquired");
-                        //queue.push(createPath(active, nextNodes[i].current, nextNodes[i].value + active.value));
+                    if(!isExist(history, nextNodes[i])){
                         enqueue(queue, createPath(active, nextNodes[i].current, nextNodes[i].value + active.value));
-                        //console.log(queue);
-                        //queue.enqueue(new Path(active, nextNodes[i].current, nextNodes[i].value + active.value));
-                        //console.log("MASUK");
+                    }
+                    else{
+                        //console.log(`${active.current} is already in history!`);
                     }
     
                 }
             }
+
+            console.log("QUEUE");
+            console.log(queue);
         }
     }
 
